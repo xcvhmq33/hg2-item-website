7fb205c3e6657224ffbbdee2dd5f105cf591fbce
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-12-10 20:42:53 +0300

    :heavy_plus_sign: Add alembic

---
+++ backend/alembic/env.py
@@ -0,0 +1,50 @@
+from logging.config import fileConfig
+
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+from app import models
+from app.core.config import settings
+target_metadata = models.Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+def get_url():
+    return str(settings.SQLALCHEMY_DATABASE_URI)
+
+url = get_url()
+connectable = create_async_engine(url)
+
+async def run_migrations():
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+if context.is_offline_mode():
+    context.configure(url=url, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+else:
+    import asyncio
+    asyncio.run(run_migrations())
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,23 +1,19 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from app.core.config import settings
-from app.models import Base
-from app.schemas import UserCreateSchema
 from app import crud
-from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.config import settings
+from app.schemas import UserCreateSchema
 
 engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI.unicode_string())
 
 
 async def init_db(session: AsyncSession) -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        user = await crud.get_user_by_name(session, settings.FIRST_SUPERUSER_NAME)
-        if user is None:
-            user_in = UserCreateSchema(
-                email=settings.FIRST_SUPERUSER_EMAIL,
-                name=settings.FIRST_SUPERUSER_NAME,
-                password=settings.FIRST_SUPERUSER_PASS,
-                is_superuser=True,
-            )
-            user = await crud.create_user(session, user_in)
+    existing_user = await crud.get_user_by_name(session, settings.FIRST_SUPERUSER_NAME)
+    if existing_user is None:
+        user_in = UserCreateSchema(
+            email=settings.FIRST_SUPERUSER_EMAIL,
+            name=settings.FIRST_SUPERUSER_NAME,
+            password=settings.FIRST_SUPERUSER_PASS,
+            is_superuser=True,
+        )
+        await crud.create_user(session, user_in)
