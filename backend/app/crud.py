aee9d110df7cb882326f79c6feaebcfb10ccf838
xcvhmq33 <98145397+xcvhmq33@users.noreply.github.com>
2024-12-10 13:13:35 +0300

    :art: Rename db to session in crud

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -8,8 +8,7 @@ from app.models import Item, User
 from app.schemas import ItemCreateSchema, UserCreateSchema
 
 
-# TODO: rename db to session
-async def create_item(db: AsyncSession, item_in: ItemCreateSchema) -> Item:
+async def create_item(session: AsyncSession, item_in: ItemCreateSchema) -> Item:
     db_item = Item(
         ingame_id=item_in.ingame_id,
         title_id=item_in.title_id,
@@ -19,40 +18,40 @@ async def create_item(db: AsyncSession, item_in: ItemCreateSchema) -> Item:
         damage_type=item_in.damage_type,
         rarity=item_in.rarity,
     )
-    db.add(db_item)
-    await db.commit()
-    await db.refresh(db_item)
+    session.add(db_item)
+    await session.commit()
+    await session.refresh(db_item)
     return db_item
 
 
-async def create_user(db: AsyncSession, user_in: UserCreateSchema) -> User:
+async def create_user(session: AsyncSession, user_in: UserCreateSchema) -> User:
     db_user = User(
         email=user_in.email,
         name=user_in.name,
         hashed_password=get_password_hash(user_in.password),
     )
-    db.add(db_user)
-    await db.commit()
-    await db.refresh(db_user)
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
     return db_user
 
 
-async def get_user_by_name(db: AsyncSession, username: str) -> User | None:
+async def get_user_by_name(session: AsyncSession, username: str) -> User | None:
     query = select(User).where(User.name == username)
-    result = await db.execute(query)
+    result = await session.execute(query)
     user = result.scalars().first()
     return user
 
 
-async def get_user_by_email(db: AsyncSession, email: str):
+async def get_user_by_email(session: AsyncSession, email: str):
     query = select(User).where(User.email == email)
-    result = await db.execute(query)
+    result = await session.execute(query)
     user = result.scalars().first()
     return user
 
 
-async def get_user_by_uuid(db: AsyncSession, uuid: uuid.UUID):
+async def get_user_by_uuid(session: AsyncSession, uuid: uuid.UUID):
     query = select(User).where(User.id == uuid)
-    result = await db.execute(query)
+    result = await session.execute(query)
     user = result.scalars().first()
     return user
